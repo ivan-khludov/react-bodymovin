28bea53477c0f8d3fbdca333dddf9024af076741
Oliver Caldwell <olliec87@gmail.com>
2017-03-22 17:07:06 +0000

    Pre compile into a single file

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     filename: 'bundle.js',
     // the output bundle
 
-    path: resolve(__dirname, 'dev/dist'),
+    path: resolve(__dirname, 'dist'),
 
     publicPath: '/'
     // necessary for HMR to know where to load the hot update chunks
