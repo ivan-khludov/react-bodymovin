eda030615ee6bddc4c18dca07ff114a1f77f16f0
acidicX <carsten.meier@peerigon.com>
2017-08-17 16:20:56 +0200

    feat(core): support SSR by not loading bodymovin on server and return an empty component

--- src/ReactBodymovin.js
+++ src/ReactBodymovin.js
@@ -1,15 +1,20 @@
 const React = require('react')
 var bodymovin;
-//Use Bodymovin Light
-if (typeof BODYMOVIN_EXPRESSION_SUPPORT === 'undefined' || BODYMOVIN_EXPRESSION_SUPPORT == null || BODYMOVIN_EXPRESSION_SUPPORT === false) {
+const isDOM = typeof window === "object" && typeof window.document === "object";
+
+//Use Bodymovin Light if running in browser
+if (isDOM && (typeof BODYMOVIN_EXPRESSION_SUPPORT === 'undefined' || BODYMOVIN_EXPRESSION_SUPPORT == null || BODYMOVIN_EXPRESSION_SUPPORT === false)) {
   bodymovin = require('bodymovin/build/player/bodymovin_light.min');
 //Use Bodymovin w/ expressions
-} else if (BODYMOVIN_EXPRESSION_SUPPORT === true){
+} else if (isDOM && BODYMOVIN_EXPRESSION_SUPPORT === true){
   bodymovin = require('bodymovin/build/player/bodymovin.min');
 }
 
 class ReactBodymovin extends React.Component {
   componentDidMount () {
+    if(!isDOM) {
+      return;
+    }
     const options = Object.assign({}, this.props.options)
     options.wrapper = this.wrapper
     options.renderer = 'svg'
@@ -17,6 +22,9 @@ class ReactBodymovin extends React.Component {
   }
 
   componentWillUnmount () {
+    if(!isDOM) {
+      return;
+    }
     this.animation.destroy()
   }
 
@@ -25,6 +33,9 @@ class ReactBodymovin extends React.Component {
   }
 
   render () {
+    if(!isDOM) {
+      return null;
+    }
     const storeWrapper = (el) => {
       this.wrapper = el
     }
